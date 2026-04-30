9d40783088b80ca80e6dcc3637474852e52af337
reudismam <reudismam@gmail.com>
2026-04-30 08:50:24 -0300

    manipulando contador.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+    
+      <body className="min-h-full flex flex-col">
+        <nav>
+          <button>Redes sociais</button>
+        </nav>
+        {children}
+        </body>
     </html>
   );
 }
