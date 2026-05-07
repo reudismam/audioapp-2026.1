db6c82d428999c46172f53b01386cd1124a82a96
reudismam <reudismam@gmail.com>
2026-05-07 08:18:08 -0300

    06-05

---
+++ app/data/musics.ts
@@ -0,0 +1,25 @@
+import Music from "./Music";
+
+const musics: Music[] =
+[
+    {
+        nome: "Áudio1",
+        url: "audios/audio1.mp3",
+        imagem: "images/audio1.jpg",
+        artistas: "Banca X"
+    },
+    {
+        nome: "Áudio2",
+        url: "audios/audio2.wav",
+        imagem: "images/audio2.jpg",
+        artistas: "Banca Y"
+    },
+    {
+        nome: "Áudio3",
+        url: "audios/audio3.wav",
+        imagem: "images/audio3.jpg",
+        artistas: "Banca Z"
+    }
+]
+
+export default musics;
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,99 @@
 
 "use client"
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+import Music from "./data/Music";
+import musics from "./data/musics";
 
 export default function Home() {
-  const [contador, setContador] = useState<number>(0);
+  const [playing, isPlaying] = useState<boolean>(false);
+  const [volume, setVolume] = useState<number>(1);
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [audioIndex, setAudioIndex] = useState<number>(0);
 
-  const increment = () => {
-    setContador(contador + 1);
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    if (playing) {
+      play();
+    }
+  }, [audioIndex]);
+
+  useEffect(() => {
+    configAudio(0);
+  }, []);
+
+  const play = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    console.log("play" + audio);
+    audio.play();
   }
 
-  const decrement = () => {
-    setContador(contador - 1);
+  const pause = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    console.log("pause" + audio);
+    audio.pause();
+  }
+
+  const playPause = () => {
+    if (playing) {
+      pause();
+    }
+    else {
+      play();
+    }
+    isPlaying(!playing);
+  }
+
+  const configVolume = (value: number) => {
+    const audio = audioRef.current;
+    if (!audio) return;
+    audio.volume = value;
+    setVolume(value);
+  }
+
+  const configAudio = (index: number) => {
+    const newIndex = index % musics.length;
+    setAudioIndex(newIndex);
+  }
+
+  const nextMusic = () => {
+    setAudioIndex(audioIndex + 1);
   }
 
   return (
-    <div className="items-center flex flex-col rounded-2xl border-gray-500 border-2 w-50 m-0 mr-auto ml-auto">
-      <p>{contador}</p>
-      <div className="flex gap-2">
-        <button onClick={()=> decrement()} className="bg-blue-300">Decrementar</button>
-        <button onClick={()=> increment()} className="bg-blue-300">Incrementar</button>
+    <div className="w-2xl mr-auto ml-auto flex">
+      <div>
+        <ul>
+          {
+            musics.map((music, index)=> {
+              return (
+                <div onClick={() => configAudio(index)} className="w-50" key={index}>
+                  <img src={musics[index].imagem} alt={musics[index].nome} />
+                </div>
+              )
+            })
+          }
+        </ul>
+      </div>
+      <div className="items-center flex flex-col rounded-2xl border-gray-500 border-2 w-50 m-0 mr-auto ml-auto">
+        <audio ref={audioRef} src={musics[audioIndex].url} controls hidden></audio>
+        <button onClick={() => playPause()} >
+          {
+            playing ? <FaPauseCircle /> : <FaPlayCircle />
+          }
+        </button>
+        <input type="range"
+          min={0}
+          max={1}
+          step={0.001}
+          value={volume}
+          onChange={(e) => configVolume(Number(e.target.value))}
+        />
+        <button onClick={() => nextMusic()}>Next</button>
       </div>
     </div>
   );
